a499d1f61dbf12e3b84fd61bfac9b57072743964
unknown <pinglin@wur.nl>
2024-02-06 13:35:16 +0100

    new device model

--- plantscreen/models/device_models.py
+++ plantscreen/models/device_models.py
@@ -1,67 +1,67 @@
+from __future__ import annotations
 from dataclasses import dataclass
 from typing import List
 from typing import Any
-import json
 
 
-# Device baseclass
 @dataclass
 class Device:
-    DeviceCaption: str
-    DeviceConfig: str
-    DeviceFamily: str
-    DeviceID: int
-    DeviceName: str
-    DevicePID: str
-    DeviceType: str
-    DeviceValidityEnd: str
-    DeviceValidityStart: str
-    ProfileID: int
+    """Device baseclass"""
+    device_caption: str
+    device_config: str
+    device_family: str
+    device_id: int
+    device_name: str
+    devicep_id: str
+    device_type: str
+    device_validity_end: str
+    device_validity_start: str
+    profile_id: int
 
     @staticmethod
-    def from_dict(obj: Any) -> 'Device':
+    def from_dict(obj: Any) -> Device:
         return Device(
-            DeviceCaption=obj.get("DeviceCaption"),
-            DeviceConfig=obj.get("DeviceConfig"),
-            DeviceFamily=obj.get("DeviceFamily"),
-            DeviceID=obj.get("DeviceID"),
-            DeviceName=obj.get("DeviceName"),
-            DevicePID=obj.get("DevicePID"),
-            DeviceType=obj.get("DeviceType"),
-            DeviceValidityEnd=obj.get("DeviceValidityEnd"),
-            DeviceValidityStart=obj.get("DeviceValidityStart"),
-            ProfileID=obj.get("ProfileID")
+            device_caption=obj.get("DeviceCaption"),
+            device_config=obj.get("DeviceConfig"),
+            device_family=obj.get("DeviceFamily"),
+            device_id=obj.get("DeviceID"),
+            device_name=obj.get("DeviceName"),
+            devicep_id=obj.get("DevicePID"),
+            device_type=obj.get("DeviceType"),
+            device_validity_end=obj.get("DeviceValidityEnd"),
+            device_validity_start=obj.get("DeviceValidityStart"),
+            profile_id=obj.get("ProfileID")
         )
-    
-# Device by ID
+
+
 @dataclass
 class DeviceWrapper:
-    Device: Device
+    """Device by ID"""
 
     @staticmethod
-    def from_dict(obj: Any) -> 'Device':
+    def from_dict(obj: Any) -> Device:
         if obj.get("JsonDeviceResult") is None:
             return None
         return Device.from_dict(obj.get("JsonDeviceResult"))
 
-# List active devices
+
 @dataclass
 class DeviceActive:
-    Device: Device
+    """List active devices"""
 
     @staticmethod
-    def from_dict(obj: Any) -> 'Device':
+    def from_dict(obj: Any) -> List[Device]:
         if obj.get("JsonDeviceActiveResult") is None:
             return []
         return [Device.from_dict(y) for y in obj.get("JsonDeviceActiveResult")]
 
-# Device by profile ID
+
 @dataclass
 class DeviceProfile:
-    Device: Device
+    """Device by profile ID"""
 
     @staticmethod
-    def from_dict(obj: Any) -> 'Device':
+    def from_dict(obj: Any) -> List[Device]:
         if obj.get("JsonDeviceByProfileIDResult") is None:
             return []
         return [Device.from_dict(y) for y in obj.get("JsonDeviceByProfileIDResult")]
