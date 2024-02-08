57ac673ed78c618f4d2f5fbd3913ed05e8bfcb7c
unknown <pinglin@wur.nl>
2024-02-08 11:48:08 +0100

    debugging

--- plantscreen/models/device_models.py
+++ plantscreen/models/device_models.py
@@ -12,7 +12,7 @@ class Device:
     device_family: str
     device_id: int
     device_name: str
-    devicep_id: str
+    device_pid: str
     device_type: str
     device_validity_end: str
     device_validity_start: str
@@ -26,7 +26,7 @@ class Device:
             device_family=obj.get("DeviceFamily"),
             device_id=obj.get("DeviceID"),
             device_name=obj.get("DeviceName"),
-            devicep_id=obj.get("DevicePID"),
+            device_pid=obj.get("DevicePID"),
             device_type=obj.get("DeviceType"),
             device_validity_end=obj.get("DeviceValidityEnd"),
             device_validity_start=obj.get("DeviceValidityStart"),
